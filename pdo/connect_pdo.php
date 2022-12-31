c51a0c3b2b2ad48e57c55a49931f39e71975585b
DESKTOP-Q3NHGSD\kuragasaki <kuragasakikenji@gmail.com>
2022-12-31 12:46:08 +0900

    staticクラスに変更

--- pdo/connect_pdo.php
+++ pdo/connect_pdo.php
@@ -1,16 +1,19 @@
 <?php
+class ConnectPdo {
+    // ドライバ呼び出しを使用して MySQL データベースに接続します
+    $dsn = 'mysql:dbname=shop;host=127.0.0.1';
+    $user = 'root';
+    $password = 'root';
 
-// ドライバ呼び出しを使用して MySQL データベースに接続します
-$dsn = 'mysql:dbname=shop;host=127.0.0.1';
-$user = 'root';
-$password = 'root';
-
-try {
-    $dbh = new PDO($dsn, $user, $password);
-    echo "接続成功\n";
-} catch (PDOException $e) {
-    echo "接続失敗: " . $e->getMessage() . "\n";
-    exit();
+    
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        echo "接続成功\n";
+    } catch (PDOException $e) {
+        echo "接続失敗: " . $e->getMessage() . "\n";
+        exit();
+    }
 }
+
 
 ?>
